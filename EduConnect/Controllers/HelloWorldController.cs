e4d6c3fd9ecade43f029f21df7f83819289cc031
vnhatnam <nhatnam18112004@gmail.com>
2025-06-02 19:59:20 +0700

    Hello World

---
+++ EduConnect/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduConnect.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+
+}
