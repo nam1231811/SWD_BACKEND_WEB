4e7ee6fb2ca9ff2a21228395753d988f47060af7
dotrongnghia2101 <dotrongnghia2101@gmail.com>
2025-06-02 20:03:22 +0700

    set name of project in swagger

--- EduConnect/Program.cs
+++ EduConnect/Program.cs
@@ -5,15 +5,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "EduConnect API",
+        Version = "v1",
+        Description = "API documentation for EduConnect system"
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "EduConnect API v1");
+        c.DocumentTitle = "EduConnect Swagger UI";
+    });
 }
 
 app.UseHttpsRedirection();
