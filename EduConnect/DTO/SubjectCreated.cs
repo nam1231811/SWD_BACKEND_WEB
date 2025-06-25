9d9f0b737f0dc015df66f62828a3a201aacb66ac
vnhatnam <nhatnam18112004@gmail.com>
2025-06-25 14:42:03 +0700

    Fix Fields in Subject

--- EduConnect/DTO/SubjectCreated.cs
+++ EduConnect/DTO/SubjectCreated.cs
@@ -4,6 +4,6 @@
     {
         public string? SubjectId { get; set; }
         public string? SubjectName { get; set; }
-        public string? TermId { get; set; }
+        public string? SemeId { get; set; }
     }
 }
