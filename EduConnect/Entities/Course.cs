7985f8b5ac54fb18f8e4310c8733696ff5937b5c
vnhatnam <nhatnam18112004@gmail.com>
2025-07-01 10:41:19 +0700

    Change Data Type (TimeOnly -> DateTime) in Course

--- EduConnect/Entities/Course.cs
+++ EduConnect/Entities/Course.cs
@@ -13,9 +13,9 @@ public partial class Course
 
     public String? SemeId { get; set; }
 
-    public TimeOnly? StartTime { get; set; }
+    public DateTime? StartTime { get; set; }
 
-    public TimeOnly? EndTime { get; set; }
+    public DateTime? EndTime { get; set; }
 
     public string? DayOfWeek { get; set; }
 
