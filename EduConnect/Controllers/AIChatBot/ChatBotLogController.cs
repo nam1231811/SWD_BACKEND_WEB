87b8a93e27749afd4992f888c1f14e8f50c77ba8
vnhatnam <nhatnam18112004@gmail.com>
2025-07-15 11:22:28 +0700

    Add save to database down SQL Server

--- EduConnect/Controllers/AIChatBot/ChatBotLogController.cs
+++ EduConnect/Controllers/AIChatBot/ChatBotLogController.cs
@@ -1,4 +1,5 @@
 ﻿using EduConnect.DTO;
+using EduConnect.Repositories;
 using EduConnect.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,11 +12,16 @@ namespace EduConnect.Controllers.AIChatBot
     {
         private readonly GroqService _groqService;
         private readonly StudentStatusService _studentStatusService;
+        private readonly IChatBotLogRepository _chatBotLogRepository;
 
-        public ChatBotLogController(GroqService groqService, StudentStatusService studentStatusService)
+        public ChatBotLogController(
+            GroqService groqService,
+            StudentStatusService studentStatusService,
+            IChatBotLogRepository chatBotLogRepository)
         {
             _groqService = groqService;
             _studentStatusService = studentStatusService;
+            _chatBotLogRepository = chatBotLogRepository;
         }
 
         [HttpPost("ask")]
@@ -24,6 +30,8 @@ namespace EduConnect.Controllers.AIChatBot
             var prompt = await _studentStatusService.GenerateFullStudentContextAsync(dto.ParentId);
             var fullPrompt = $"{prompt}\n\nCâu hỏi của phụ huynh: {dto.MessageText}";
             var reply = await _groqService.AskAsync(fullPrompt);
+
+            await _chatBotLogRepository.CreateLogAsync(dto.ParentId, dto.MessageText, reply);
 
             return Ok(new { reply });
         }
